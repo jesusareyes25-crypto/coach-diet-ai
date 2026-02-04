55bcd1ac274b90d03e1163f6fb6253b14daf38f8
jesusareyes25-crypto <jesusareyes25@gmail.com>
2026-02-04 18:05:44 -0500

    v8

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   serverExternalPackages: ["@react-pdf/renderer", "sharp"],
-  serverExternalPackages: ["@react-pdf/renderer", "sharp"],
 };
 
 export default nextConfig;
