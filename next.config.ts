3dfb6a11ee84bd0ce1c7fa7e3bc08daddf1d9f65
jesusareyes25-crypto <jesusareyes25@gmail.com>
2026-02-04 17:59:16 -0500

    v6

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   serverExternalPackages: ["@react-pdf/renderer", "sharp"],
-  webpack: (config) => {
-    config.resolve.alias.canvas = false;
-    return config;
-  },
+  serverExternalPackages: ["@react-pdf/renderer", "sharp"],
 };
 
 export default nextConfig;
